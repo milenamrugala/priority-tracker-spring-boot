99de40dae8be041d0e29b9de53aaf8cd53e981e4
milenamrugala <milena.mrugala@icloud.com>
2023-05-29 22:31:58 +0200

    initial commit

---
+++ src/main/java/pl/milenamrugala/prioritytrackerspringboot/PriorityTrackerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.milenamrugala.prioritytrackerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorityTrackerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriorityTrackerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/milenamrugala/prioritytrackerspringboot/PriorityTrackerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milenamrugala.prioritytrackerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriorityTrackerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
