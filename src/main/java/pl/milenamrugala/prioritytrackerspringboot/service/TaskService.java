771ce7a9d2f6a7df3a4d95c1bf6ab11ee68852b9
milenamrugala <milena.mrugala@icloud.com>
2023-05-30 18:13:15 +0200

    TaskService created

---
+++ src/main/java/pl/milenamrugala/prioritytrackerspringboot/service/TaskService.java
@@ -0,0 +1,34 @@
+package pl.milenamrugala.prioritytrackerspringboot.service;
+
+import org.springframework.stereotype.Service;
+import pl.milenamrugala.prioritytrackerspringboot.model.Task;
+import pl.milenamrugala.prioritytrackerspringboot.repository.TaskRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+       return taskRepository.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTaskById(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
