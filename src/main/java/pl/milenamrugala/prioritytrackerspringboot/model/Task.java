1445e46946b3b2f9dcea577365c03128dfcb1f3f
milenamrugala <milena.mrugala@icloud.com>
2023-07-04 15:39:39 +0200

    Task class changes

--- src/main/java/pl/milenamrugala/prioritytrackerspringboot/model/Task.java
+++ src/main/java/pl/milenamrugala/prioritytrackerspringboot/model/Task.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -23,7 +24,9 @@ public class Task {
     private Long id;
     private String description;
     private String priority;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endDate;
 
 }
