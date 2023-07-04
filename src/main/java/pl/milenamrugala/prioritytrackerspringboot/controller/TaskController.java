008014396a30eebe8f0b60957479ba3f09bda34e
milenamrugala <milena.mrugala@icloud.com>
2023-07-04 15:39:52 +0200

    TaskController changes

--- src/main/java/pl/milenamrugala/prioritytrackerspringboot/controller/TaskController.java
+++ src/main/java/pl/milenamrugala/prioritytrackerspringboot/controller/TaskController.java
@@ -3,28 +3,65 @@ package pl.milenamrugala.prioritytrackerspringboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.milenamrugala.prioritytrackerspringboot.model.Task;
 import pl.milenamrugala.prioritytrackerspringboot.service.TaskService;
 
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/priority-tracker")
 
 public class TaskController {
-    private final TaskService taskService;
+
+    private TaskService taskService;
 
     @Autowired
     public TaskController(TaskService taskService) {
+        super();
         this.taskService = taskService;
     }
 
     @GetMapping("/list-tasks")
     public String listTasks(Model model) {
-        List<Task> tasks = taskService.getAllTasks();
-        model.addAttribute("tasks", tasks);
+        model.addAttribute("tasks", taskService.getAllTasks());
         return "list-tasks";
     }
+
+    @GetMapping("/add-task")
+    public String addTask(Model model) {
+        Task task = new Task();
+        model.addAttribute("task", task);
+        return "add-task";
+    }
+
+    @PostMapping("/add-task")
+    public String saveTask(@ModelAttribute("task") Task task) {
+        taskService.saveTask(task);
+        return "redirect:/priority-tracker/list-tasks";
+    }
+
+    @GetMapping("/list-tasks/update")
+    public String updateTask(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("task", taskService.getTaskById(id));
+        return "edit-task";
+    }
+
+    @PostMapping("/list-tasks/update")
+    public String saveEditTask(@ModelAttribute("task") Task task) {
+        taskService.saveTask(task);
+        return "redirect:/priority-tracker/list-tasks";
+    }
+
+    @GetMapping("/list-tasks/delete")
+    public String deleteTask(@RequestParam("id") Long id, Model model) {
+        taskService.deleteById(id);
+        return "redirect:/priority-tracker/list-tasks";
+    }
+
+    @GetMapping("/list-tasks/delete-confirm")
+    public String confirmDeleteTask(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("id", id);
+        return "task-delete-confirm";
+    }
+
 }
